9aeb5819c2dec0b0336a29f8b5d7e2ef7a4fe4c5
Elias Bjartur <eliaseinars@gmail.com>
2022-09-22 16:41:50 +0000

    Hlaða fzf completion og key-bindings. kóperaðar skrár. Sourcast ekki

--- fzf.bash
+++ fzf.bash
@@ -6,8 +6,10 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+# [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+source "~/.shell/completion.bash"
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+# source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+source "~/.shell/key-bindings.bash"
