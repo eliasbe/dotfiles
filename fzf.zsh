3b0209ae99b47634d1525ab3de39c2dd48a9d104
Elias Bjartur <eliaseinars@gmail.com>
2023-05-12 15:43:50 +0000

    Improve fasd z using fzf if no argument

--- fzf.zsh
+++ fzf.zsh
@@ -98,6 +98,14 @@ alg() {
     file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vi "${file}" || return 1
 }
 
+# fasd & fzf change directory - jump using `fasd` if given argument, filter output of `fasd` using `fzf` else
+unalias z
+z() {
+    [ $# -gt 0 ] && fasd_cd -d "$*" && return
+    local dir
+    dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
+
 
 ### Git
 # fshow - git commit browser
