ca023309ead09648c537be6b2e86df6b6e8b5486
Elias Bjartur <eliaseinars@gmail.com>
2024-03-05 15:29:16 +0000

    Fix git add in fzf-git
    Note, the header is cluttered and sometimes out of place

--- shell/fzf/fzf.zsh
+++ shell/fzf/fzf.zsh
@@ -49,8 +49,9 @@ _fzf_git_fzf() {
     --border-label-pos=2 \
     --color='header:italic:underline,label:blue' \
     --preview-window='right,50%,border-left' \
-    --bind='ctrl-s:change-preview-window(down,50%,border-top|hidden|)' "$@" \
-    --header='CTRL-O (open in browser) ╱ ALT-E (open in editor) ╱ CTRL-S (change preview) ╱ (SHIFT) TAB (select)'
+    --bind='ctrl-s:change-preview-window(down,50%,border-top|hidden|)' \
+    --bind='ctrl-a:execute(echo {} | awk "{print \$2}" | xargs -I % git add % &)' "$@" \
+    --header='CTRL-O (open in browser) ╱ ALT-E (open in editor) ╱ CTRL-S (change preview) ╱ (SHIFT) TAB (select) ╱ CTRL-A (git add)'
 }
 
 # Use fd command for listing path candidates.
